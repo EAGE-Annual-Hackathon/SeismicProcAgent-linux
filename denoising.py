0678779dec84157dce9082d4f8139b573acc987d
JiahuaZhao <j_zhao_cyi@instance-20250521-052213.europe-west4-c.c.research-459618.internal>
2025-06-02 10:09:52 +0000

    Update

--- denoising.py
+++ denoising.py
@@ -111,6 +111,117 @@ async def denoise_svd_with_cutoff(data_path: str, data: str, line_number: int, l
     
     return Image(out_file)
 
+@mcp.tool()
+async def median_denoise(data_path: str, data: str, line_number: int, line_type: str, size: tuple = (2, 2)):
+
+    """
+    Denoises a matrix using median filter.
+
+    Args:
+    data_path: Path to the seismic record file.
+    data: Seismic record file, the suffix is usually .sgy or .segy.
+    line_number: Inline or crossline number 
+    line_type: "inline" or "crossline"
+    size: Size of the median filter
+    """
+
+    mdio_path = os.path.join(data_path, f"{data.split('.')[0]}.mdio")
+
+    mdio = MDIOReader(mdio_path, return_metadata=True)
+    
+    inlines = mdio.grid.select_dim("inline").coords
+    crosslines = mdio.grid.select_dim("crossline").coords
+    times = mdio.grid.select_dim("sample").coords
+    std = mdio.stats["std"]
+
+    if line_type == "inline":
+        inline_index = mdio.coord_to_index(line_number, dimensions="inline").item()
+        _, _, data = mdio[inline_index]
+    
+    elif line_type == "crossline":
+        crossline_index = mdio.coord_to_index(line_number, dimensions="crossline").item()
+        _, _, data = mdio[:, crossline_index]
+        
+        
+    # Plot original and filtered versions
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
+    vmin, vmax = data.min(), data.max()
+    # median
+    median_denoised = median_filter(data, size)
+
+    axes[0].imshow(data.T, cmap='seismic', aspect='auto', vmin=vmin, vmax=vmax)
+    axes[0].set_title("Original")
+
+    axes[1].imshow(median_denoised.T, cmap='seismic', aspect='auto', vmin=vmin, vmax=vmax)
+    axes[1].set_title("Median Filtered")
+
+    axes[2].imshow(data.T-median_denoised.T, cmap='seismic', aspect='auto', vmin=vmin, vmax=vmax)
+    axes[2].set_title("Median misfit")
+    for ax in axes:
+        ax.set_xlabel('Trace')
+    axes[0].set_ylabel('Time/Depth Sample')
+    plt.tight_layout()
+
+    out_file = f"denoised_median_inline_{line_number}_{size}.jpg"
+    plt.savefig(out_file)
+    plt.close()
+    
+    return Image(out_file)
+
+@mcp.tool()
+async def gaussian_denoise(data_path: str, data: str, line_number: int, line_type: str, sigma: float = 1.0):
+    """
+    Denoises a matrix with Gaussian.
+
+    Args:
+    data_path: Path to the seismic record file.
+    data: Seismic record file, the suffix is usually .sgy or .segy.
+    line_number: Inline or crossline number
+    line_type: "inline" or "crossline"
+    sigma: Standard deviation of the Gaussian filter    
+    """
+
+    mdio_path = os.path.join(data_path, f"{data.split('.')[0]}.mdio")
+
+    mdio = MDIOReader(mdio_path, return_metadata=True)
+    
+    inlines = mdio.grid.select_dim("inline").coords
+    crosslines = mdio.grid.select_dim("crossline").coords
+    times = mdio.grid.select_dim("sample").coords
+    std = mdio.stats["std"]
+
+    if line_type == "inline":
+        inline_index = mdio.coord_to_index(line_number, dimensions="inline").item()
+        _, _, data = mdio[inline_index]
+    
+    elif line_type == "crossline":
+        crossline_index = mdio.coord_to_index(line_number, dimensions="crossline").item()
+        _, _, data = mdio[:, crossline_index]
+    
+    # Plot original and filtered versions
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
+    vmin, vmax = data.min(), data.max()
+    # gaussian
+    gaussian_denoised = gaussian_filter(data, sigma)
+
+    axes[0].imshow(data.T, cmap='seismic', aspect='auto', vmin=vmin, vmax=vmax)
+    axes[0].set_title("Original")
+
+    axes[1].imshow(gaussian_denoised.T, cmap='seismic', aspect='auto', vmin=vmin, vmax=vmax)
+    axes[1].set_title("Gaussian Filtered")
+
+    axes[2].imshow(data.T-gaussian_denoised.T, cmap='seismic', aspect='auto', vmin=vmin, vmax=vmax)
+    axes[2].set_title("Gaussian misfit")
+    for ax in axes:
+        ax.set_xlabel('Trace')
+    axes[0].set_ylabel('Time/Depth Sample')
+    plt.tight_layout()
+
+    out_file = f"denoised_gaussian_inline_{line_number}_{sigma}.jpg"
+    plt.savefig(out_file)
+    plt.close()
+    
+    return Image(out_file)
 
 if __name__ == "__main__":
     # Initialize and run the server
